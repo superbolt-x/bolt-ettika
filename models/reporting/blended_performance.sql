d3e0675c27875e7cfca31310c5b294aa9433fc1f
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2026-05-11 18:49:11 +0200

    Add adset_id and ad_id to blended_performance.sql

--- models/reporting/blended_performance.sql
+++ models/reporting/blended_performance.sql
@@ -75,6 +75,18 @@ WITH orders AS (
             THEN campaign_id
             ELSE '(not set)'
         END AS campaign_id,
+		CASE 
+            WHEN source IN ('facebook-ads','google-ads')
+                 AND adset_id != ''
+            THEN adset_id
+            ELSE '(not set)'
+        END AS adset_id,
+		CASE 
+            WHEN source IN ('facebook-ads','google-ads')
+                 AND ad_id != ''
+            THEN ad_id
+            ELSE '(not set)'
+        END AS ad_id,
 		index,
 		count(*) over (partition by order_id) as order_index
 
@@ -93,6 +105,8 @@ WITH orders AS (
         r.date,
         COALESCE(t.channel,'Other') AS channel,
         COALESCE(t.campaign_id,'(not set)') AS campaign_id,
+		COALESCE(t.adset_id,'(not set)') AS adset_id,
+		COALESCE(t.ad_id,'(not set)') AS ad_id,
 
         r.order_id,
         r.customer_order_index,
@@ -128,6 +142,8 @@ WITH orders AS (
         date,
         channel,
         campaign_id,
+		adset_id,
+		ad_id,
         SUM(
             sales_amount::float / order_index::float
         ) AS revenue,
@@ -165,67 +181,80 @@ WITH orders AS (
         ) AS new_purchases
 
     FROM attributed_data
-    GROUP BY 1,2,3
+    GROUP BY 1,2,3,4,5
 
 )
 
 , paid_objects as (
-	SELECT 'Facebook' as channel, campaign_id::varchar as campaign_id, campaign_name, count(*) as nb
+	SELECT 'Facebook' as channel, campaign_id::varchar as campaign_id, campaign_name, 
+	adset_id::varchar as adset_id, adset_name, ad_id::varchar as ad_id, ad_name, count(*) as nb
 	FROM reporting.ettika_facebook_ad_performance 
-	group by 1,2,3
+	group by 1,2,3,4,5,6,7
 	union all
-	SELECT 'Google Ads' as channel, campaign_id::varchar as campaign_id, campaign_name, count(*) as nb
+	SELECT 'Google Ads' as channel, campaign_id::varchar as campaign_id, campaign_name,
+	'(not set)' as adset_id, '(not set)' as adset_name, '(not set)' as ad_id, '(not set)' as ad_name, count(*) as nb
 	FROM reporting.ettika_googleads_campaign_performance
-	group by 1,2,3
+	WHERE campaign_name ~* 'Pmax'
+	group by 1,2,3,4,5,6,7
 	union all
-	SELECT 'App Lovin' as channel, campaign_id_external::varchar as campaign_id, campaign as campaign_name, count(*) as nb
+	SELECT 'Google Ads' as channel, campaign_id::varchar as campaign_id, campaign_name, 
+	ad_group_id::varchar as adset_id, ad_group_name, ad_id::varchar as ad_id, ad_name, count(*) as nb
+	FROM reporting.ettika_googleads_ad_performance
+	group by 1,2,3,4,5,6,7
+	union all
+	SELECT 'App Lovin' as channel, campaign_id_external::varchar as campaign_id, campaign as campaign_name,
+	'(not set)' as adset_id, '(not set)' as adset_name, '(not set)' as ad_id, '(not set)' as ad_name, count(*) as nb
 	FROM applovin_raw.advertiser_report
 	WHERE "_fivetran_deleted" = FALSE
-	group by 1,2,3
+	group by 1,2,3,4,5,6,7
 )
 
 , conversion_data as (
-	select * from final_sho_data left join paid_objects using(channel,campaign_id)
+	select * from final_sho_data left join paid_objects using(channel,campaign_id,adset_id,ad_id)
 )
 
 , paid_data as (
 	SELECT 'Facebook' as channel, campaign_id::varchar as campaign_id, campaign_name,
+	adset_id::varchar as adset_id, adset_name, ad_id::varchar as ad_id, ad_name,
 	date, sum(spend) as spend, sum(impressions) as impressions, sum(link_clicks) as clicks
 	FROM reporting.ettika_facebook_ad_performance 
 	where date_granularity = 'day'
 	and (spend > 0 or link_clicks > 0 or impressions > 0)
-	group by 1,2,3,4
+	group by 1,2,3,4,5,6,7,8
 	union all
 	SELECT 'Google Ads' as channel, campaign_id::varchar as campaign_id, campaign_name,
+	'(not set)' as adset_id, '(not set)' as adset_name, '(not set)' as ad_id, '(not set)' as ad_name,
 	date, sum(spend) as spend, sum(impressions) as impressions, sum(clicks) as clicks
 	FROM reporting.ettika_googleads_campaign_performance
 	where date_granularity = 'day'
 	and (spend > 0 or clicks > 0 or impressions > 0)
 	and campaign_name ~* 'Pmax'
-	group by 1,2,3,4
+	group by 1,2,3,4,5,6,7,8
 	union all
 	SELECT 'Google Ads' as channel, campaign_id::varchar as campaign_id, campaign_name,
+	ad_group_id::varchar as adset_id, ad_group_name, ad_id::varchar as ad_id, ad_name,
 	date, sum(spend) as spend, sum(impressions) as impressions, sum(clicks) as clicks
 	FROM reporting.ettika_googleads_ad_performance
 	where date_granularity = 'day'
 	and (spend > 0 or clicks > 0 or impressions > 0)
-	group by 1,2,3,4
+	group by 1,2,3,4,5,6,7,8
 	union all
-	SELECT 'App Lovin' as channel, campaign_id_external::varchar as campaign_id, campaign as campaign_name, 
+	SELECT 'App Lovin' as channel, campaign_id_external::varchar as campaign_id, campaign as campaign_name,
+	'(not set)' as adset_id, '(not set)' as adset_name, '(not set)' as ad_id, '(not set)' as ad_name,
 	date_trunc('day',"day") as date, SUM(CAST(REPLACE(REPLACE(cost, '$', ''), ',', '') AS FLOAT)) as spend, 
 	sum(impressions) as impressions, sum(clicks) as clicks
 	FROM applovin_raw.advertiser_report
 	WHERE "_fivetran_deleted" = FALSE
-	group by 1,2,3,4
+	group by 1,2,3,4,5,6,7,8
 )
 
 , final_data AS (
 SELECT 
-	date::date as date, channel, campaign_id, campaign_name,
+	date::date as date, channel, campaign_id, campaign_name, adset_id, adset_name, ad_id, ad_name,
 	coalesce(spend,0) as spend, coalesce(impressions,0) as impressions, coalesce(clicks,0) as clicks, 
 	coalesce(net_revenue,0) as net_revenue, coalesce(revenue,0) as revenue, coalesce(new_revenue,0)-coalesce(new_refunds,0) as net_new_revenue, coalesce(new_revenue,0) as new_revenue, coalesce(purchases,0) as purchases, coalesce(new_purchases,0) as new_purchases
 FROM conversion_data 
-FULL OUTER JOIN paid_data USING(date,channel,campaign_id,campaign_name)
+FULL OUTER JOIN paid_data USING(date,channel,campaign_id,campaign_name,adset_id,adset_name,ad_id,ad_name)
 ORDER BY date desc )
 
 select *
